8152f92d05065de551af5e359026f7b0f2bfa533
George Dragan <george.dragan@qcatalyst.com>
2020-11-14 13:34:55 +0200

    Basic refactoring

--- blocks_of_colours.py
+++ blocks_of_colours.py
@@ -16,57 +16,62 @@ class Table:
         self.columns = columns
         self.contents = []
         colours = {0:"Red", 1:"Green", 2:"Blue"}
-        for row in range(rows):
+        for tree_nodes in range(rows):
             row_list = []
             for column in range(columns):
                 colour = colours[random.randint(0, 2)]
-                new_tree_node = TreeNode(colour, [row, column])
+                new_tree_node = TreeNode(colour, [tree_nodes, column])
                 row_list.append(new_tree_node)
             self.contents.append(row_list)
 
     def print_table(self):
-        for i_row in range(self.rows):
-            row_list = self.contents[i_row]
-            row_list_values = []
-            for i_column in range(self.columns):
-                row_list_values.append(row_list[i_column].value)
-            print(row_list_values)
+        # for i_row in range(self.rows):
+        #     row_list = self.contents[i_row]
+        #     row_list_values = []
+        #     for i_column in range(self.columns):
+        #         row_list_values.append(row_list[i_column].value)
+        #     print(row_list_values)
+        for tree_nodes in self.contents:
+            print(' '.join([tree_node.value.ljust(7) for tree_node in tree_nodes]))
 
     def get_left_neighbor(self, adress):
-        row = adress[0]
+        tree_nodes = adress[0]
         column = adress[1]
         if column == 0:
             return None
         else:
-            row_list = self.contents[row]
+            row_list = self.contents[tree_nodes]
             return row_list[column - 1]
 
     def get_right_neighbor(self, adress):
-        row = adress[0]
+        tree_nodes = adress[0]
         column = adress[1]
-        row_list = self.contents[row]
+        row_list = self.contents[tree_nodes]
         try:
             return row_list[column + 1]
         except IndexError:
             pass
 
     def get_top_neighbor(self, adress):
-        row = adress[0]
+        tree_nodes = adress[0]
         column = adress[1]
-        if row == 0:
+        if tree_nodes == 0:
             return None
         else:
-            row_list = self.contents[row - 1]
+            row_list = self.contents[tree_nodes - 1]
             return row_list[column]
 
     def get_bottom_neighbor(self, adress):
-        row = adress[0]
+        tree_nodes = adress[0]
         column = adress[1]
         try:
-            row_list = self.contents[row + 1]
+            row_list = self.contents[tree_nodes + 1]
             return row_list[column]
         except IndexError:
             pass
+    
+    def get_neighbors(self, adress):
+        return [self.get_bottom_neighbor(adress), self.get_left_neighbor(adress), self.get_top_neighbor(adress), self.get_right_neighbor(adress)]
 
 def in_tree_dict(current_node):
     for value in tree_dict.values():
@@ -84,21 +89,25 @@ tabel.print_table()
 
 print(roots_list)
 
-for row in tabel.contents:
-    for tree_node in row:
-        current_node = tree_node
-        current_node_adress = current_node.adress
-        current_node_color = current_node.value
-        current_node_adress = current_node.adress
-        bottom_neighbor = tabel.get_bottom_neighbor(current_node_adress)
-        left_neighbor = tabel.get_left_neighbor(current_node_adress)
-        top_neighbor = tabel.get_top_neighbor(current_node_adress)
-        right_neighbor = tabel.get_right_neighbor(current_node_adress)
-        neighbors_list = [bottom_neighbor, left_neighbor, top_neighbor, right_neighbor]
-        if current_node_adress not in roots_list:
-            if not in_tree_dict(current_node):
-                for neighbor in neighbors_list:
-                    try:
+def get_max_coverage():
+
+    for tree_nodes in tabel.contents:
+        for tree_node in tree_nodes:
+            current_node = tree_node
+
+            current_node_adress = current_node.adress
+            current_node_color = current_node.value
+            current_node_adress = current_node.adress
+
+            # filter None
+            neighbors_list = [neighbor for neighbor in table.get_neighbors(current_node_adress) if neighbor]
+
+            # unpaking
+            # bottom_neighbor, left_neighbor, top_neighbor, right_neighbor = table.get_neighbors(current_node_adress)
+
+            if current_node_adress not in roots_list:
+                if not in_tree_dict(current_node):
+                    for neighbor in neighbors_list:
                         if neighbor.value == current_node.value:
                             if neighbor.adress in roots_list:
                                 tree_dict[neighbor].append(neighbor)
@@ -108,47 +117,47 @@ for row in tabel.contents:
                         else:
                             roots_list.append(current_node.adress)
                             tree_dict[current_node] = []
-                    except AttributeError:
-                        pass
-            else:
-                for neighbor in neighbors_list:
-                    try:
-                        if neighbor.adress in roots_list or in_tree_dict(neighbor):
-                            continue
-                        else:
-                            try:
-                                if neighbor.value == current_node.value:
-                                    current_node_root = current_node.root
-                                    tree_dict[current_node_root].append(neighbor)
-                                    neighbor.root = current_node_root
-                            except AttributeError:
-                                pass
-                    except AttributeError:
-                        pass
-        for neighbor in neighbors_list:
-            try:
-                if neighbor.adress in roots_list or in_tree_dict(neighbor):
-                    continue
+                        
                 else:
-                    try:
-                        if neighbor.value == current_node.value:
-                            tree_dict[current_node].append(neighbor)
-                            neighbor.root = current_node
-                    except AttributeError:
-                        pass
-            except AttributeError:
-                pass
-group_count = {"Red":0, "Blue":0, "Green":0}
+                    for neighbor in neighbors_list:
+                        try:
+                            if neighbor.adress in roots_list or in_tree_dict(neighbor):
+                                continue
+                            else:
+                                try:
+                                    if neighbor.value == current_node.value:
+                                        current_node_root = current_node.root
+                                        tree_dict[current_node_root].append(neighbor)
+                                        neighbor.root = current_node_root
+                                except AttributeError:
+                                    pass
+                        except AttributeError:
+                            pass
 
-#for key in tree_dict.keys():
-#    colour = key.value
-#    group_count[colour] += 1
-#    group_size = len(tree_dict[key]) + 1
-#    print("Grupul nr {0} din culoarea {1} are marimea {2}".format(group_count[colour], colour, group_size))
+            for neighbor in neighbors_list:
+                try:
+                    if neighbor.adress in roots_list or in_tree_dict(neighbor):
+                        continue
+                    else:
+                        try:
+                            if neighbor.value == current_node.value:
+                                tree_dict[current_node].append(neighbor)
+                                neighbor.root = current_node
+                        except AttributeError:
+                            pass
+                except AttributeError:
+                    pass
 
-max_group = max(tree_dict.values(), key = len)
-colour = max_group[0].value
-root = max_group[0].root
-root_adress = root.adress
-print("\nCel mai mare grup este de culoarea {0}\nAre un total de {1} elemente\nRadacina grupului se afla pe pozitia {2}\n".format(colour, len(max_group) + 1, root_adress))
+    group_count = {"Red":0, "Blue":0, "Green":0}
+    max_group = max(tree_dict.values(), key = len)
+    colour = max_group[0].value
+    root = max_group[0].root
+    root_adress = root.adress
+    
+    return (colour, root_adress, len(max_group) + 1)
+    
+
+colour, root_adress, group_lenght = get_max_coverage()
+
+print(f"\nCel mai mare grup este de culoarea {colour}\nAre un total de {group_lenght} elemente\nRadacina grupului se afla pe pozitia {root_adress}\n")
     
